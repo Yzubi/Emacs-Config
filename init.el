e947a5335599caa8dbcfa04929de49559483730f
Yzubi <42348748+Yzubi@users.noreply.github.com>
2019-08-03 17:24:36 -0300

    Add files via upload

--- init.el
+++ init.el
@@ -118,9 +118,10 @@
 (ido-mode 1)
 
 ;; Search
+(global-unset-key ["C-s"])
 (global-set-key (kbd "C-f") 'isearch-forward)
 (define-key isearch-mode-map (kbd "C-f") 'isearch-repeat-forward)
-(global-unset-key ["C-s"])
+(define-key isearch-mode-map (kbd "C-s") 'isearch-repeat-backward)
 
 ;; Suspend frame
 (global-set-key (kbd "s-s") 'suspend-frame)
@@ -157,6 +158,18 @@
 ;; Shift number
 (global-set-key (kbd "M-+") 'shift-number-up)
 (global-set-key (kbd "M-_") 'shift-number-down)
+
+(global-set-key (kbd "<f5>") 'set-selective-display-dlw)
+
+(defun set-selective-display-dlw (&optional level)
+"Fold text indented same of more than the cursor.
+If level is set, set the indent level to LEVEL.
+If 'selective-display' is already set to LEVEL, clicking
+F5 again will unset 'selective-display' by setting it to 0."
+  (interactive "P")
+  (if (eq selective-display (1+ (current-column)))
+      (set-selective-display 0)
+    (set-selective-display (or level (1+ (current-column))))))
 
 ;; Custom set variables
 (custom-set-variables
