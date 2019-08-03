812aaaef49ef95731b81ade54b468f5e8971d36d
Yzubi <42348748+Yzubi@users.noreply.github.com>
2019-08-03 17:26:04 -0300

    Add files via upload

--- init.el
+++ init.el
@@ -159,18 +159,6 @@
 (global-set-key (kbd "M-+") 'shift-number-up)
 (global-set-key (kbd "M-_") 'shift-number-down)
 
-(global-set-key (kbd "<f5>") 'set-selective-display-dlw)
-
-(defun set-selective-display-dlw (&optional level)
-"Fold text indented same of more than the cursor.
-If level is set, set the indent level to LEVEL.
-If 'selective-display' is already set to LEVEL, clicking
-F5 again will unset 'selective-display' by setting it to 0."
-  (interactive "P")
-  (if (eq selective-display (1+ (current-column)))
-      (set-selective-display 0)
-    (set-selective-display (or level (1+ (current-column))))))
-
 ;; Custom set variables
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
