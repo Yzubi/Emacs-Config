ea3152d9092179d7b5ea6e4051b7dfeee3b86e2a
Yzubi <42348748+Yzubi@users.noreply.github.com>
2019-08-14 05:23:54 -0300

    Update init.el

--- init.el
+++ init.el
@@ -213,7 +213,8 @@ using the specified hippie-expand function."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cua-keep-region-after-copy t))
+ '(cua-keep-region-after-copy t)
+ '(custom-enabled-themes (quote (wombat))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
