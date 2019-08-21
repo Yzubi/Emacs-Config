6f6dcc0810985dbb0d54fd4b54b5fdcdbf59cd91
Yzubi <42348748+Yzubi@users.noreply.github.com>
2019-08-21 03:44:28 -0300

    Update init.el

--- init.el
+++ init.el
@@ -109,11 +109,12 @@
 ;; Change indent style - Hackish improved Allman
 (setq c-default-style "bsd")
 (setq-default c-basic-offset 0)
-(c-set-offset 'topmost-intro         '8)
-(c-set-offset 'brace-list-intro      '8)
-(c-set-offset 'arglist-intro         '8)
-(c-set-offset 'statement-block-intro '8)
-(c-set-offset 'defun-block-intro     '8)
+(c-set-offset 'topmost-intro          '0)
+(c-set-offset 'class-open             '8)
+(c-set-offset 'brace-list-intro       '8)
+(c-set-offset 'arglist-intro          '8)
+(c-set-offset 'statement-block-intro  '8)
+(c-set-offset 'defun-block-intro      '8)
 
 ;; Enable xterm mouse mode
 (xterm-mouse-mode t)
