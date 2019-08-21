b9ee08b881065ebe11f52a133ac024e6be41a044
Yzubi <42348748+Yzubi@users.noreply.github.com>
2019-08-20 22:05:47 -0300

    Update init.el

--- init.el
+++ init.el
@@ -106,6 +106,8 @@
 (setq-default electric-indent-inhibit t)
 ;; Use spaces instead of tabs for indentation
 (setq-default indent-tabs-mode nil)
+;; Change indent style
+(setq c-default-style "bsd")
 
 ;; Enable xterm mouse mode
 (xterm-mouse-mode t)
