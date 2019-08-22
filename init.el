85f43dee09097438d6dabd53199d4c4dc959880f
Yzubi <42348748+Yzubi@users.noreply.github.com>
2019-08-22 19:37:56 -0300

    Update init.el

--- init.el
+++ init.el
@@ -403,7 +403,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(region ((t (:background "navy" :foreground "#f6f3e8"))))
+ '(region ((t (:background "#0066ff" :foreground "#f6f3e8"))))
 
  '(company-tooltip ((t (:background "#eeeeee" :foreground "#252525"))))
  '(company-tooltip-annotation ((t (:foreground "#252525"))))
